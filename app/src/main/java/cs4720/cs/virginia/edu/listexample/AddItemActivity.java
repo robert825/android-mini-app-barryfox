8bf5696d36a7840cdc74c18eb6f68f130692320b
Robert Fox <robert.fox825@gmail.com>
2018-02-05 01:18:01 -0500

    test

--- app/src/main/java/cs4720/cs/virginia/edu/listexample/AddItemActivity.java
+++ app/src/main/java/cs4720/cs/virginia/edu/listexample/AddItemActivity.java
@@ -2,12 +2,51 @@ package cs4720.cs.virginia.edu.listexample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+<<<<<<< HEAD
+=======
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.CalendarView;
+
+>>>>>>> master
 
 public class AddItemActivity extends AppCompatActivity {
 
     @Override
+<<<<<<< HEAD
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_item);
+=======
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_item);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+    }
+
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        EditText name = (EditText) findViewById(R.id.name);
+        EditText description = (EditText) findViewById(R.id.description);
+        EditText latitude = (EditText) findViewById(R.id.latitude);
+        EditText longitude = (EditText) findViewById(R.id.longitude);
+        CalendarView calendar = (CalendarView) findViewById(R.id.calendarView);
+        String dateString = Long.toString(calendar.getDate());
+        String[] messageName = {name.getText().toString(), description.getText().toString(),latitude.getText().toString(),longitude.getText().toString(), dateString};
+        intent.putExtra(EXTRA_MESSAGE, messageName);
+        startActivity(intent);
+>>>>>>> master
     }
 }
--- app/src/main/java/cs4720/cs/virginia/edu/listexample/MainActivity.java
+++ app/src/main/java/cs4720/cs/virginia/edu/listexample/MainActivity.java
@@ -7,17 +7,24 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
+<<<<<<< HEAD
 import android.view.ViewParent;
 import android.widget.CheckedTextView;
+=======
+>>>>>>> master
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.support.design.widget.FloatingActionButton;
+
+import java.io.StringBufferInputStream;
 import java.util.Date;
 import java.util.ArrayList;
+import android.content.Intent;
+import android.widget.TableRow;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     ArrayList<BucketItem> BucketList;
     EditText nameField;
     RecyclerView rvBucketLists;
@@ -33,11 +40,17 @@ public class MainActivity extends AppCompatActivity {
 //
 //        // Initialize BucketLists
         BucketList = BucketItem.createInitialBucketList();
+        Intent intent = getIntent();
+        try {
+            String[] message = intent.getStringArrayExtra(AddItemActivity.EXTRA_MESSAGE);
+            BucketItem b = new BucketItem(message[0], message[1], Double.parseDouble(message[2]), Double.parseDouble(message[3]), new Date(1220227200));
+            BucketList.add(b);
+        } catch (NullPointerException n) {
+            System.out.println("Error");
+        }
 
 //        // Create adapter passing in the sample user data
         BucketListAdapter adapter = new BucketListAdapter(this, BucketList);
-        System.out.println(adapter.toString());
-        System.out.println(BucketList.toString());
 //        // Attach the adapter to the recyclerview to populate items
         rvBucketLists.setAdapter(adapter);
 //        // Set layout manager to position the items
@@ -48,27 +61,30 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    // Called when you tap the Add BucketList button
-    public void addBucketList(View view) {
-        // Make sure it is a name
-        if(nameField.getText().toString() != null && !nameField.getText().toString().equals("")) {
-            // Log the action
-            Log.d("ListExample", "addBucketList " + nameField.getText().toString());
-            // Make a new BucketList
-            BucketList.add(new BucketItem(nameField.getText().toString(), "description coming soon", 0.0, 0.1, new Date(1220227200)));
-            // Get the adapter that manages the data set and let it know something new was added
-            rvBucketLists.getAdapter().notifyDataSetChanged();
-            // Blank the name field
-            nameField.setText("");
-        }
+
+
+
+    public void goToAddPage(View view)
+    {
+        Intent intent = new Intent(MainActivity.this, AddItemActivity.class);
+        startActivity(intent);
+
     }
 
-    // Called tapping on an online BucketList
-    public void sendMessage(View view) {
-        TextView currentItem = (TextView)view;
-        Log.d("ListExample", "sendMessage to " + currentItem.getText().toString());
-        // Make Toast
-        Toast.makeText(this, "Sending message to " + currentItem.getText().toString(), Toast.LENGTH_LONG).show();
+    public void goToEditPage(View view) {
+       // Toast.makeText(this, "Congrats! You completed an activity " , Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(MainActivity.this, EditItemActivity.class);
+        /*****We will need to change the following line*****/
+        int indexOfView = 0; //((TableRow) view.getParent()).indexOfChild(view);
+        String name = BucketList.get(indexOfView).getName();
+        String description = BucketList.get(indexOfView).getDescription();
+        String lat = Double.toString(BucketList.get(indexOfView).getLat());
+        String lon = Double.toString(BucketList.get(indexOfView).getLon());
+        String date = new Long(BucketList.get(indexOfView).getDate().getTime()).toString();
+
+        String[] messageName = {name, description, lat, lon, date};
+        intent.putExtra(EXTRA_MESSAGE, messageName);
+        startActivity(intent);
     }
 
     public void toggle_checked(View view) {
